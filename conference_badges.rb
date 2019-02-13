0746ea58d9a285df266121a58ac9ccb5eef7f62a
Thomas Edwards <thce28@gmail.com>
2019-02-13 13:21:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr = []
+  array.each { |i| arr << badge_maker(i)}
+
+  arr
+end
+
+def assign_rooms(array)
+  arr = []
+  array.each_with_index { |item, index| arr << "Hello, #{item}! You'll be assigned to room #{index + 1}!"}
+
+  arr
+end
+
+def printer(attendees)
+  attendees.each do |i|
+    
+end
+end
+    
+    
